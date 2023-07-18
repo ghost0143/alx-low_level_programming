4b7af4579e1d0e0a8df08fd3281c1e50414528a4
AJAVON Ayi Daniel <ajavon0143@gmail>
2023-07-18 16:25:31 -0700

    Write a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,26 +10,32 @@
  */
 int main(void)
 {
-	int fib1 = 1;  /* Premier nombre de Fibonacci */
-	int fib2 = 2;  /* Deuxième nombre de Fibonacci */
-	int fib;       /* Variable temporaire pour calculer le nombre suivant */
-	int count;     /* Compteur pour suivre le nombre de nombres de Fibonacci */
+    int fib1 = 1;  /* Premier nombre de Fibonacci */
+    int fib2 = 2;  /* Deuxième nombre de Fibonacci */
+    int fib;       /* Variable temporaire pour calculer le nombre suivant */
+    int count;     /* Compteur pour suivre le nombre de nombres de Fibonacci */
 
-	/* Imprimer les deux premiers nombres de Fibonacci */
-	printf("%d, %d", fib1, fib2);
+    /* Imprimer les deux premiers nombres de Fibonacci */
+    printf("%d, %d", fib1, fib2);
 
-	/* Calculer et imprimer les 48 nombres de Fibonacci suivants */
-	for (count = 3; count <= 50; count++)
-	{
-		fib = fib1 + fib2;
-		printf(", %d", fib);
+    /* Calculer et imprimer les 48 nombres de Fibonacci suivants */
+    for (count = 3; count <= 50; count++)
+    {
+        fib = fib1 + fib2;
+        printf(", %d", fib);
 
-		/* Mettre à jour les valeurs pour le calcul du prochain nombre de Fibonacci */
-		fib1 = fib2;
-		fib2 = fib;
-	}
+        /* Mettre à jour les valeurs pour le calcul du prochain nombre de Fibonacci */
+        fib1 = fib2;
+        fib2 = fib;
 
-	printf("\n");
+        /* Si la ligne dépasse 80 caractères, passer à la ligne suivante */
+        if (count % 10 == 0)
+        {
+            printf("\n");
+        }
+    }
 
-	return 0;
+    printf("\n");
+
+    return 0;
 }
