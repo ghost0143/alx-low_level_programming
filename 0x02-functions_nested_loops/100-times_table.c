d0fba469bbacaf28f3a56f7e85fcf5b75f1adad6
AJAVON Ayi Daniel <ajavon0143@gmail>
2023-07-18 18:40:56 -0700

    Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Imprime la table de multiplication jusqu'à n
- * @n: Nombre indiquant jusqu'à quelle valeur imprimer la table
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number for which the times table is printed.
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    int i;
-    int j;
-    int result;
+	int i, j, result;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            result = i * j;
-            if (result < 10)
-            {
-                if (j > 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                _putchar(result + '0');
-            }
-            else if (result < 100)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10) % 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+				printf("%d", result);
+			else
+				printf(", %3d", result);
+		}
+
+		printf("\n");
+	}
 }
