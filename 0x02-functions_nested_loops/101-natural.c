30e968afe10d1c5312c4c71d6fec3743897a6bab
AJAVON Ayi Daniel <ajavon0143@gmail>
2023-07-18 19:01:15 -0700

    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,26 +3,26 @@
 /**
  * main - Entry point
  *
- * Description: Computes and prints the sum of all the multiples
- * of 3 or 5 below 1024 (excluded).
+ * Description: Prints the first 50 Fibonacci numbers, starting with 1 and 2.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int limit = 1024;
-	int sum = 0;
-	int i;
+        int count = 50;
+        int i;
+        unsigned long long int fib1 = 1;
+        unsigned long long int fib2 = 2;
 
-	for (i = 0; i < limit; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			sum += i;
-		}
-	}
+        printf("%llu", fib1);
+        for (i = 1; i < count; i++)
+        {
+                printf(", %llu", fib2);
+                unsigned long long int next = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = next;
+        }
+        printf("\n");
 
-	printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
-
-	return (0);
+        return 0;
 }
