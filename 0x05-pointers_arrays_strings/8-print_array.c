f31292615e36f00b1bb7436f9599504db1b008b8
AJAVON Ayi Daniel <ajavon0143@gmail>
2023-07-24 18:56:24 -0700

    Write a function that prints n elements of an array of integers, followed by a new line.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
     {
         printf("%d", a[i]);
 
-        // Print a comma and a space after all elements except the last one
+        /* Print a comma and a space after all elements except the last one */
         if (i < n - 1)
         {
             printf(", ");
